12c546880dab37b074784d0bfc28dd9dee52d070
Ganesh Lohar <38778334+ganeshlohar59@users.noreply.github.com>
2021-05-25 14:31:02 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var compliencesContainer = document.getElementById("compliance");
+var policyContainer = document.getElementById("policies");
+var agreementsContainer = document.getElementById("agreements");
+
+function scrollToComplenceSection() {
+  compliencesContainer.scrollIntoView();
+}
+
+function scrollToPoliciesSection() {
+  policyContainer.scrollIntoView();
+}
+
+function scrollToAgreementsSection() {
+  agreementsContainer.scrollIntoView();
+}
